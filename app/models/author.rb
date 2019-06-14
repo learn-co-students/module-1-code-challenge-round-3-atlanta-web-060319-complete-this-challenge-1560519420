cc57f9084138587c626bcf84f69b854f1424b81c
github username <github email address>
2019-06-14 10:57:41 -0400

    needed more time ROUND 1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,41 @@
+require "pry"
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def add_article(magazine, title, content)
+    Author.new(self, magazine, title, content)
+  end
+
+  def articles
+     Articles.all.select{|article|article.author == self}
+  end
+
+  # def self.most_verbose
+  # #   # longest_article = 0
+  # #   # all_articles = Article.all.map do |article|
+  # #   #   if article.content.length > longest_article
+  # #   #     longest_article = article.content.length
+  # #   #   end
+  # #   # end
+  # #   # all_articles.map{|article|article.content.length == longest_article}
+
+  # #   #..... will come back
+  # #   Article.all.max_by(&:length)
+  # # end 
+  
+  def magazines
+    this_is = Article.all.select{|item|item.magazine}
+    this_is.map{|item|item.magazine}.uniq
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,35 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+author1 = Author.new("Sheldon")
+author2 = Author.new("Maryam the magnificient")
+author3 = Author.new("Gary")
+
+test1 = author2.name
+test2 = Author.all 
+# test3 = Author.most_verbose
+
+magazine1 = Magazine.new("Hiker's Guide", "Todo")
+magazine2 = Magazine.new("Tech Time","History")
+magazine3 = Magazine.new("Food Fanatics","Guns and Ammunition")
+
+article1 = Article.new(author2,magazine1,"Growth as a mentor","Why am I content, I could be a blurb #lifegoals")
+article2 = Article.new(author1, magazine1, "Grilling chicken","The art and science of a good piece of grilled chicken")
+article3 = Article.new(author2, magazine2, "My secret love of cookies","There is no content... carry on")
+
+test4 = Article.all
+test5 = article2.author
+
+test6 = article1.magazine
+test7 = article3.title
+test8 = article3.content
+
+test9 = magazine2.name
+test10 = magazine3.category
+test11 = Magazine.all
+test12 = Magazine.find_by_name("Tech Time")
+
+
 
 
 
