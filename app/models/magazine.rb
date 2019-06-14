401506b5e96608a36f2f36bc5cbc0ce6131478bc
github username <github email address>
2019-06-14 10:58:59 -0400

    Here goes nothin

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,43 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
+  
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self 
   end
+
+  def self.all
+    @@all
+  end 
+
+  def articles
+    Article.all.select { |art| art.magazine == self }
+  end 
+
+  def contributors
+    articles.collect { |art| art.author }
+  end 
+
+  def self.find_by_name(magname)
+    Magazine.all.find { |mag| mag.name == magname}
+  end 
+
+  def article_titles
+   articles.collect { |mag| mag.title }
+  end 
+
+  def word_count
+    total = 0
+    articles.collect { |art|
+    total += art.content.length
+  }
+    total
+  end 
+
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../config/environment.rb'
 
 def reload
@@ -6,7 +7,27 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+chris = Author.new("Chris Kalfas")
+steph = Author.new("Steph Lloyd")
 
+people = Magazine.new("People", "Gossip")
+today = Magazine.new("USA Today", "News")
+
+health = Article.new(chris, people, "Health Care Today", "Things arent looking good. Don't die")
+cats = Article.new(chris, today, "Cats are Everywhere", "Cats attacks on rise in downtown area")
+cars = Article.new(steph, people, "Cars", "Cars are smarter than the people driving them.")
+
+test1 = chris.add_article(people, "Coding is Real", "To code or not to code")
+test2 = chris.articles
+test3 = chris.magazines
+test4 = people.articles
+test5 = people.contributors
+#test6 = Author.most_verbose
+test7 = Magazine.find_by_name("USA Today")
+test8 = chris.show_specialties
+test9 = Author.most_active
+test10 = people.article_titles
+test11 = people.word_count
 
 
 
